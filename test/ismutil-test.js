f4b1b05c3ac7cbef0f1068677ae8cb3cdaf8cda4
Casey Duncan <casey.duncan@gmail.com>
2013-11-26 11:25:40 -0500

    uid generator

---
+++ test/ismutil-test.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+
+suite('genUid', function() {
+    var genUid = require('../lib/ismutil').genUid;
+
+    test('uids are sufficiently long', function(done) {
+        genUid(function(uid) {
+            assert(uid.length > 30, 'uid too short');
+            done();
+        })
+    });
+
+    test('uids are url safe', function(done) {
+        var uidUrlSafe = function(remaining) {
+            if (remaining > 0) {
+                genUid(function(uid) {
+                    assert.equal(uid, encodeURIComponent(uid), 'uid not url-safe');
+                    uidUrlSafe(--remaining);
+                });
+            } else {
+                done();
+            }
+        }
+        uidUrlSafe(100);
+    });
+
+    test('uids are unique', function() {
+        var uids = {};
+        var uidUnique = function(remaining) {
+            if (remaining > 0) {
+                genUid(function(uid) {
+                    assert(uid, 'genUid() generated empty uid');
+                    assert(!(uid in uids), 'genUid() generated duplicate uid');
+                    uidUnique(--remaining);
+                });
+            } else {
+                done();
+            }
+        }
+        uidUnique(10000);
+    });
+});
