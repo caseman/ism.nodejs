c40f9ae370e9cb3c1e588937685a4230477345d3
Casey Duncan <casey.duncan@gmail.com>
2013-12-11 23:45:18 -0800

    server accepts or creates database

--- test/db-test.js
+++ test/db-test.js
@@ -1,20 +1,40 @@
-var assert = require('assert');
+var assert = require('assert')
+  , path = require('path')
+  , fs = require('fs')
+  , tmp = require('tmp');
 
-suite('db key', function() {
-    var key = require('../lib/db').key;
+suite('db', function() {
+    var db = require('../lib/db');
 
     test('key with various string parts', function() {
-        assert.equal(key('somethin'), 'somethin');
-        assert.equal(key('somethin', 'else'), 'somethin~else');
-        assert.equal(key('somethin', 'or', 'other'), 'somethin~or~other');
-        assert.equal(key('some', 100, 'thing'), 'some~100~thing');
+        assert.equal(db.key('somethin'), 'somethin');
+        assert.equal(db.key('somethin', 'else'), 'somethin~else');
+        assert.equal(db.key('somethin', 'or', 'other'), 'somethin~or~other');
+        assert.equal(db.key('some', 100, 'thing'), 'some~100~thing');
     });
 
     test('key with uid objects', function() {
         var o1 = {uid:'1'}, o2 = {uid:'2'};
-        assert.equal(key(o1), '1');
-        assert.equal(key(o1, o2), '1~2');
-        assert.equal(key('thing', o1, 'and', o2), 'thing~1~and~2');
+        assert.equal(db.key(o1), '1');
+        assert.equal(db.key(o1, o2), '1~2');
+        assert.equal(db.key('thing', o1, 'and', o2), 'thing~1~and~2');
+    });
+
+    test('default path in home dir', function() {
+        var dbPath = db.defaultPath();
+        assert.equal(dbPath.indexOf(process.env.HOME), 0);
+    });
+
+    test('creates subdirectories on open', function(done) {
+        tmp.dir({unsafeCleanup: true}, function(err, dirPath) {
+            dbPath = path.join(dirPath, 'how', 'now');
+            assert(!fs.existsSync(dbPath));
+            var testDb = db.open(dbPath, function(err, db) {
+                assert(fs.existsSync(dbPath));
+                done();
+            });
+            assert.equal(testDb.location, dbPath);
+        });
     });
 
 });
