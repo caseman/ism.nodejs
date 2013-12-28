37cd42f2850f6a0ad6e7c2cc572973941b40efbf
Casey Duncan <casey.duncan@gmail.com>
2013-12-28 08:20:29 -0700

    use real client

--- script/debug-client.js
+++ script/debug-client.js
@@ -1,31 +1,15 @@
 #!/usr/bin/env node
-var sockjsc = require('sockjs-client');
-var version = require('../package.json').version;
+var logging = require('../lib/logging')
+  , client = require('../client/client');
 
-var client = sockjsc.create('http://0.0.0.0:5557/ism');
+logging.configure(true, true);
 
-client.on('connection', function() {
-    console.log('connected');
+var myClient = client.create(5557, '0.0.0.0');
+
+myClient.on('connected', function() {
+    myClient.handshake(function(whatev) {
+        console.log('handshake successful', whatev);
+    });
 });
-
-client.on('data', function(msg) {
-    console.log('received:', msg);
-});
-
-client.on('error', function(err) {
-    console.log('error:', err);
-});
-
-client.on('close', function() {
-    console.log('connection closed');
-});
-
-function send(obj) {
-    var data = JSON.stringify(obj)
-    client.write(data);
-    console.log('sent:', data);
-}
-
-send({says:'hi', uid:'hello', clientVersion:version});
 
 
