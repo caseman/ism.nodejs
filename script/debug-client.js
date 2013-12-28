86fc8db35435b05b8cd779a8d055413eb989899d
Casey Duncan <casey.duncan@gmail.com>
2013-12-28 08:29:43 -0700

    improve output

--- script/debug-client.js
+++ script/debug-client.js
@@ -7,8 +7,8 @@ logging.configure(true, true);
 var myClient = client.create(5557, '0.0.0.0');
 
 myClient.on('connected', function() {
-    myClient.handshake(function(whatev) {
-        console.log('handshake successful', whatev);
+    myClient.handshake(function() {
+        console.log('handshake successful cid =', this.cid);
     });
 });
 
