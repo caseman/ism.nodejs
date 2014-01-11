22be7c05e310f552920e430f068a54477b9ad8f6
Casey Duncan <casey.duncan@gmail.com>
2014-01-11 14:30:34 -0800

    Create, join and start a game when run

--- script/debug-client.js
+++ script/debug-client.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 var logging = require('../lib/logging')
-  , client = require('../client/client');
+  , client = require('../lib/client')
+  , util = require('util');
 
 logging.configure(true, true);
 logging.useFile(logging.defaultClientPath());
 
 var myClient = client.create(5557, '0.0.0.0');
 
-myClient.on('connected', function() {
+function gameCreated(game) {
+    console.log('\nCreated game', game);
+    myClient.send({says:'join', game:game.uid});
+    myClient.send({says:'startGame', game:game.uid});
+}
+
+myClient.on('connection', function() {
     myClient.handshake(function() {
         console.log('handshake successful cid =', this.cid);
+        myClient.createGame(['small'], gameCreated, function(progress) {
+            util.print('.');
+        });
     });
 });
 
