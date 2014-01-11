be6a33f2ea7540a380adec559b3ed534f7805066
Casey Duncan <casey.duncan@gmail.com>
2014-01-11 14:30:02 -0800

    get options from file and cmd line args escape to exit

--- script/debug-app.js
+++ script/debug-app.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 var logging = require('../lib/logging')
-  , app = require('../termclient/app');
+  , app = require('../termclient/app')
+  , ui = require('../termclient/ui')
+  , extend = require('extend')
+  , argv = require('optimist').argv;
 
 logging.configure(true, true);
 logging.useFile(logging.defaultClientPath());
 
-var app = app.create();
+var fileOpts = require(process.env.HOME + '/.ism/client-options.json');
+var app = app.create(extend({}, fileOpts, argv));
 app.start();
+
+ui.screen.key('escape', process.exit);
