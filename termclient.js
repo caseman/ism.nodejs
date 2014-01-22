8eecf08928083e5798130772347606c318df4e41
Casey Duncan <casey.duncan@gmail.com>
2014-01-21 17:09:43 -0700

    front-end script for terminal client

---
+++ termclient.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+var prefs = require('./lib/prefs')
+  , path = require('path')
+  , opt = require('optimist')
+    .usage('Terminal client for the Ism strategy game\nUsage: $0')
+    .alias('v', 'verbose')
+    .describe('v', 'More verbose logging')
+    .boolean('v')
+    .describe('debug', 'Debug logging of all client/server activity')
+    .boolean('debug')
+    .describe('version', 'Output the software version and exit')
+    .boolean('version')
+    .describe('prefs', 'Path to prefs file (will be created if necessary)')
+
+if (prefs.defaultClientPath()) {
+    opt.default('prefs', prefs.defaultClientPath())
+} else {
+    opt.demand('prefs')
+}
+
+var argv = opt.argv
+
+if (argv.version) {
+    var clientVersion = require('./package.json').version
+    console.log('ism version ' + clientVersion)
+    console.log('node version ' + process.version)
+    process.exit()
+}
+
+if (argv.help) {
+    opt.showHelp()
+    process.exit()
+}
+
+var logging = require('./lib/logging')
+logging.configure(argv.verbose || argv.debug, argv.debug)
+logging.useFile(path.join(path.dirname(argv.prefs), 'client.log'))
+
+prefs.usePath(argv.prefs)
+
+require('./termclient/app').start()
